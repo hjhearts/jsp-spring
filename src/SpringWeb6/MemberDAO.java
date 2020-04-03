dbaf7c10cbf6722cb2c8ba76876d995b4fe81f2b
hjhearts <60081459+hjhearts@users.noreply.github.com>
2020-04-03 11:55:41 +0900

    first upload

--- src/SpringWeb6/MemberDAO.java
+++ src/SpringWeb6/MemberDAO.java
@@ -4,8 +4,6 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.log4j.lf5.util.Resource;
-import org.apache.tomcat.util.file.ConfigurationSource;
 
 import java.io.Reader;
 import java.util.List;
