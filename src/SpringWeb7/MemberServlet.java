9dfb7aeb87b850c754c3eb30ff78e1805c270d83
hjhearts <60081459+hjhearts@users.noreply.github.com>
2020-04-03 13:37:31 +0900

    member.xml 의 mapping을 변경하여 SpringWeb7 패키지가 동작

---
+++ src/SpringWeb7/MemberServlet.java
@@ -0,0 +1,39 @@
+package SpringWeb7;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/mem2.do")
+public class MemberServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doHandle(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doHandle(req, resp);
+    }
+    protected void doHandle(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        MemberDAO dao = new MemberDAO();
+        String name = dao.selectName();
+        int pwd = dao.selectPwd();
+        PrintWriter out = response.getWriter();
+        out.println("<script>");
+        out.println("alert('이름 : " + name + "');");
+        out.println("alert('패스워드 : " + pwd + "');");
+        out.println("</script>");
+    }
+}
